d6cea30c5817283ef456e496147d7b5dfff99669
Yinghao-Li <www6com2010@hotmail.com>
2023-03-27 11:53:55 -0400

    minor update

---
+++ mubench/unimol/model/module.py
@@ -0,0 +1,108 @@
+import torch
+import torch.nn as nn
+from .layers import utils
+
+
+class ClassificationHead(nn.Module):
+    """Head for sentence-level classification tasks."""
+
+    def __init__(
+            self,
+            input_dim,
+            inner_dim,
+            num_classes,
+            activation_fn,
+            pooler_dropout,
+    ):
+        super().__init__()
+        self.dense = nn.Linear(input_dim, inner_dim)
+        self.activation_fn = utils.get_activation_fn(activation_fn)
+        self.dropout = nn.Dropout(p=pooler_dropout)
+        self.out_proj = nn.Linear(inner_dim, num_classes)
+
+    def forward(self, features, **kwargs):
+        x = features[:, 0, :]  # take <s> token (equiv. to [CLS])
+        x = self.dropout(x)
+        x = self.dense(x)
+        x = self.activation_fn(x)
+        x = self.dropout(x)
+        x = self.out_proj(x)
+        return x
+
+
+class NonLinearHead(nn.Module):
+    """Head for simple classification tasks."""
+
+    def __init__(
+            self,
+            input_dim,
+            out_dim,
+            activation_fn,
+            hidden=None,
+    ):
+        super().__init__()
+        hidden = input_dim if not hidden else hidden
+        self.linear1 = nn.Linear(input_dim, hidden)
+        self.linear2 = nn.Linear(hidden, out_dim)
+        self.activation_fn = utils.get_activation_fn(activation_fn)
+
+    def forward(self, x):
+        x = self.linear1(x)
+        x = self.activation_fn(x)
+        x = self.linear2(x)
+        return x
+
+
+class DistanceHead(nn.Module):
+    def __init__(
+            self,
+            heads,
+            activation_fn,
+    ):
+        super().__init__()
+        self.dense = nn.Linear(heads, heads)
+        self.layer_norm = nn.LayerNorm(heads)
+        self.out_proj = nn.Linear(heads, 1)
+        self.activation_fn = utils.get_activation_fn(activation_fn)
+
+    def forward(self, x):
+        bsz, seq_len, seq_len, _ = x.size()
+        # x[x == float('-inf')] = 0
+        x = self.dense(x)
+        x = self.activation_fn(x)
+        x = self.layer_norm(x)
+        x = self.out_proj(x).view(bsz, seq_len, seq_len)
+        x = (x + x.transpose(-1, -2)) * 0.5
+        return x
+
+
+@torch.jit.script
+def gaussian(x, mean, std):
+    pi = 3.14159
+    a = (2 * pi) ** 0.5
+    return torch.exp(-0.5 * (((x - mean) / std) ** 2)) / (a * std)
+
+
+class GaussianLayer(nn.Module):
+    def __init__(self, k=128, edge_types=1024):
+        super().__init__()
+        self.K = k
+        self.means = nn.Embedding(1, k)
+        self.stds = nn.Embedding(1, k)
+        self.mul = nn.Embedding(edge_types, 1)
+        self.bias = nn.Embedding(edge_types, 1)
+        nn.init.uniform_(self.means.weight, 0, 3)
+        nn.init.uniform_(self.stds.weight, 0, 3)
+        nn.init.constant_(self.bias.weight, 0)
+        nn.init.constant_(self.mul.weight, 1)
+
+    def forward(self, x, edge_type):
+        mul = self.mul(edge_type).type_as(x)
+        bias = self.bias(edge_type).type_as(x)
+        x = mul * x.unsqueeze(-1) + bias
+        x = x.expand(-1, -1, -1, self.K)
+        mean = self.means.weight.float().view(-1)
+        std = self.stds.weight.float().view(-1).abs() + 1e-5
+        return gaussian(x.float(), mean, std).type_as(self.means.weight)
+
+
