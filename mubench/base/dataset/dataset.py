update label scaling and fix a bug

mubench/base/dataset/dataset.py
@@ -26,27 +26,35 @@ class Dataset(BaseDataset):
 
         self._features = None
         self._smiles: Union[List[str], None] = None
-        self._lbs: Union[List[List[Union[int, float]]], None] = None
-        self._masks: Union[List[List[int]], None] = None
+        self._lbs: Union[np.ndarray, None] = None
+        self._masks: Union[np.ndarray, None] = None
     
     @property
     def features(self):
         return self._features
 
     @property
-    def smiles(self):
+    def smiles(self) -> List[str]:
         return self._smiles
     
     @property
-    def lbs(self):
+    def lbs(self) -> np.ndarray:
         return self._lbs
 
     @cached_property
-    def masks(self):
+    def masks(self) -> np.ndarray:
         return self._masks if self._masks is not None else np.ones_like(self.lbs).astype(int)
 
     def __len__(self):
         return len(self._smiles)
+
+    def update_lbs(self, lbs):
+        """
+        Update dataset labels and instance list accordingly
+        """
+        self._lbs = lbs
+        self.data_instances = self.get_instances()
+        return self
 
     def prepare(self, config, partition, **kwargs):
         """

mubench/base/train.py
@@ -5,6 +5,7 @@ Base trainer function.
 """
 
 import os
+import copy
 import torch
 import wandb
 import logging
@@ -22,6 +23,7 @@ from torch.utils.data import DataLoader
 
 from ..utils.macro import EVAL_METRICS
 from ..utils.container import ModelContainer, UpdateCriteria
+from ..utils.scaler import StandardScaler
 from .metric import (
     GaussianNLL,
     calculate_classification_metrics,
@@ -40,13 +42,15 @@ class Trainer:
                  training_dataset=None,
                  valid_dataset=None,
                  test_dataset=None,
-                 collate_fn=None):
+                 collate_fn=None,
+                 scalar=None):
 
         self._config = config
         self._training_dataset = training_dataset
         self._valid_dataset = valid_dataset
         self._test_dataset = test_dataset
         self._collate_fn = collate_fn if collate_fn is not None else Collator(config)
+        self._scalar = scalar
         self._device = getattr(config, "device", "cpu")
 
         self._model = None
@@ -75,6 +79,10 @@ class Trainer:
             self.config.uncertainty_method,
         )
 
+        # normalize training dataset labels for regression task
+        self.check_and_update_training_dataset()
+
+        # initialize training modules
         self.initialize()
 
     @property
@@ -161,6 +169,22 @@ class Trainer:
     @property
     def test_dataset(self):
         return self._test_dataset
+
+    def check_and_update_training_dataset(self):
+        if self.config.task_type == 'classification':
+            return self
+        if self._training_dataset is None and self._scalar is None:
+            logger.warning("Encounter regression task with no training dataset specified and label scaling disabled! "
+                           "This may create inconsistency between training and inference label scales.")
+            return self
+
+        lbs = copy.deepcopy(self._training_dataset.lbs)
+        lbs[~self._training_dataset.masks.astype(bool)] = np.nan
+        self._scalar = StandardScaler(replace_nan_token=0).fit(lbs)
+
+        self._training_dataset.update_lbs(self._scalar.transform(self._training_dataset.lbs))
+
+        return self
 
     @cached_property
     def n_training_steps(self):
@@ -299,21 +323,29 @@ class Trainer:
             else:
                 preds = expit(logits)  # sigmoid function
         else:
+            # get the mean of the preds
             preds = logits if logits.shape[-1] == 1 or len(logits.shape) == 1 else logits[..., 0]
+
+        return preds
+
+    def scale_back_lbs(self, preds: np.ndarray) -> np.ndarray:
+        if self._scalar is None:
+            return preds
+        preds = self._scalar.inverse_transform(preds)
         return preds
 
     def evaluate(self, dataset, n_run: Optional[int] = 1, return_preds: Optional[bool] = False):
 
         if n_run == 1:
 
-            preds = self.normalize_logits(self.inference(dataset))
+            preds = self.scale_back_lbs(self.normalize_logits(self.inference(dataset)))
             metrics = self.get_metrics(dataset.lbs, preds, dataset.masks)
 
         else:
             preds = list()
             for i_run in (tqdm_run := tqdm(range(n_run))):
                 tqdm_run.set_description(f'[Test {i_run}]')
-                preds.append(self.normalize_logits(self.inference(dataset)))
+                preds.append(self.scale_back_lbs(self.normalize_logits(self.inference(dataset))))
             preds = np.stack(preds)
             metrics = self.get_metrics(dataset.lbs, preds.mean(axis=0), dataset.masks)
 
@@ -334,7 +366,7 @@ class Trainer:
         self.log_results(valid_results, logging_func=logger.debug)
 
         # ----- check model performance and update buffer -----
-        if self._model_container.check_and_update(getattr(valid_results, self._valid_metric), self.model):
+        if self._model_container.check_and_update(self.model, getattr(valid_results, self._valid_metric)):
             logger.debug("Model buffer is updated!")
 
         return None

mubench/utils/container.py
@@ -2,10 +2,13 @@ import copy
 import regex
 import torch
 import logging
+import numpy as np
 from enum import Enum
 from typing import Optional, Union
 
 logger = logging.getLogger(__name__)
+
+__all__ = ['UpdateCriteria', 'ModelContainer']
 
 
 class UpdateCriteria(str, Enum):
@@ -29,12 +32,12 @@ class ModelContainer:
         ----------
         update_criteria: decides whether the metrics are in descend order
         """
-        self._state_dict = None
-        self._metric = None
-
         assert update_criteria in UpdateCriteria.get_options(), \
             ValueError(f"Invalid criteria! Options are {UpdateCriteria.get_options()}")
         self._criteria = update_criteria
+
+        self._state_dict = None
+        self._metric = np.inf if self._criteria == UpdateCriteria.metric_smaller else -np.inf
 
     @property
     def state_dict(self):
@@ -44,7 +47,7 @@ class ModelContainer:
     def metric(self):
         return self._metric
 
-    def check_and_update(self, metric: Union[int, float], model) -> bool:
+    def check_and_update(self, model, metric: Optional[Union[int, float]] = None) -> bool:
         """
         Check whether the new model performs better than the buffered models.
         If so, replace the worst model in the buffer by the new model
@@ -63,6 +66,7 @@ class ModelContainer:
                       (self._criteria == UpdateCriteria.metric_larger and metric >= self.metric)
 
         if update_flag:
+            self._metric = metric
             model_cp = copy.deepcopy(model)
             model_cp.to('cpu')
 

mubench/utils/scaler.py
@@ -3,12 +3,15 @@ The scaler for the regression task.
 This implementation is adapted from
 https://github.com/chemprop/chemprop/blob/master/chemprop/data/scaler.py
 """
-from typing import Any, List, Union
+from typing import Any
 import numpy as np
+
+__all__ = ['StandardScaler']
 
 
 class StandardScaler:
-    """A StandardScaler normalizes a dataset.
+    """
+    A StandardScaler normalizes a dataset.
 
     When fit on a dataset, the StandardScaler learns the mean and standard deviation across the 0th axis.
     When transforming a dataset, the StandardScaler subtracts the means and divides by the standard deviations.
@@ -28,7 +31,7 @@ class StandardScaler:
         self.stds = stds
         self.replace_nan_token = replace_nan_token
 
-    def fit(self, x: Union[List[List[float]], np.ndarray]) -> 'StandardScaler':
+    def fit(self, x: np.ndarray) -> 'StandardScaler':
         """
         Learns means and standard deviations across the 0th axis.
 
@@ -49,7 +52,7 @@ class StandardScaler:
 
         return self
 
-    def transform(self, x: Union[List[List[float]], np.ndarray]):
+    def transform(self, x: np.ndarray):
         """
         Transforms the data by subtracting the means and dividing by the standard deviations.
 
@@ -67,7 +70,7 @@ class StandardScaler:
 
         return transformed_with_none
 
-    def inverse_transform(self, x: Union[List[List[float]], np.ndarray]):
+    def inverse_transform(self, x:  np.ndarray):
         """
         Performs the inverse transformation by multiplying by the standard deviations and adding the means.
 
@@ -82,8 +85,9 @@ class StandardScaler:
         if isinstance(x, np.ndarray) or isinstance(x, list):
             x = np.array(x).astype(float)
             transformed_with_nan = x * self.stds + self.means
-            transformed_with_none = np.where(np.isnan(transformed_with_nan),
-                                             self.replace_nan_token, transformed_with_nan)
+            transformed_with_none = np.where(
+                np.isnan(transformed_with_nan), self.replace_nan_token, transformed_with_nan
+            )
             return transformed_with_none
         else:
             return None