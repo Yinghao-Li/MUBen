5aedc5a452debe158e986186dfa7a69072e43e16
Yinghao-Li <www6com2010@hotmail.com>
2023-05-25 13:14:23 -0400

    update scripts

--- scripts/run_grover_ld.sh
+++ scripts/run_grover_ld.sh
@@ -6,18 +6,19 @@ set -e
 # --- toggle the following command to decide whether train the model on each dataset ---
 # --- the argument values do not matter ---
 
+
 # -- regression tasks --
 # train_on_esol=true
 # train_on_freesolv=true
 # train_on_lipo=true
 # train_on_qm7=true
-# train_on_qm8=true
+train_on_qm8=true
 train_on_qm9=true
 
 # -- single-task classification --
 # train_on_bbbp=true
 # train_on_bace=true
-train_on_hiv=true
+# train_on_hiv=true
 
 # -- multi-task classification --
 # train_on_tox21=true
@@ -36,29 +37,26 @@ wandb_api_key="86efcc8aa38a82bd8128db4c1cee3acde0f33920"
 disable_wandb=false
 
 data_folder="./data/files"
-unimol_feature_folder="./data/UniMol"
 num_workers=16
 num_preprocess_workers=16
 pin_memory=false
-ignore_preprocessed_dataset=true
+ignore_preprocessed_dataset=false
 
 uncertainty_method="DeepEnsembles"  # this is subject to change
-retrain_model=true
+retrain_model=false
 
 binary_classification_with_softmax=false
 regression_with_variance=true
 
-# default hyper-parameters
-lr=0.00005
-batch_size=32
-batch_size_inference=16
-n_epochs=100
+lr=0.0001
+batch_size=64
+n_epochs=150
 valid_tolerance=40
 
 seed=0
 
 # Uncertainty arguments
-n_test=1
+n_test=30
 n_ensembles=3
 
 # --- universal arguments region ends ---
@@ -80,37 +78,17 @@ if [ -z ${train_on_qm8+x} ]; then echo "skip qm8"; else dataset_names+=" qm8"; f
 if [ -z ${train_on_hiv+x} ]; then echo "skip hiv"; else dataset_names+=" hiv"; fi
 if [ -z ${train_on_muv+x} ]; then echo "skip muv"; else dataset_names+=" muv"; fi
 if [ -z ${train_on_qm9+x} ]; then echo "skip qm9"; else dataset_names+=" qm9"; fi
-if [ -z ${train_on_pcab+x} ]; then echo "skip pcab"; else dataset_names+=" pcab"; fi
+if [ -z ${train_on_pcba+x} ]; then echo "skip pcba"; else dataset_names+=" pcba"; fi
 
 # --- training scripts ---
 for dataset_name in $dataset_names
 do
-  if [ $dataset_name == "bbbp" ]
-  then
-    lr=0.0001
-    n_epochs=20
-  fi
-  if [ $dataset_name == "sider" ]
-  then
-    lr=0.0001
-  fi
-  if [ $dataset_name == "hiv" ]
-  then
-    batch_size=64
-    n_epochs=20
-  fi
-  if [ $dataset_name == "muv" ]
-  then
-    batch_size=64
-    n_epochs=40
-  fi
-  CUDA_VISIBLE_DEVICES=$cuda_device python run_unimol.py \
+  CUDA_VISIBLE_DEVICES=$cuda_device python run_grover.py \
     --wandb_api_key $wandb_api_key \
     --disable_wandb $disable_wandb \
     --data_folder $data_folder \
-    --unimol_feature_folder $unimol_feature_folder \
     --dataset_name "$dataset_name" \
-    --checkpoint_path ./models/unimol_base.pt \
+    --checkpoint_path ./models/grover_base.pt \
     --num_workers $num_workers \
     --num_preprocess_workers $num_preprocess_workers \
     --pin_memory $pin_memory \
@@ -123,7 +101,6 @@ do
     --n_epochs $n_epochs \
     --valid_tolerance $valid_tolerance \
     --batch_size $batch_size \
-    --batch_size_inference $batch_size_inference \
     --seed $seed \
     --n_test $n_test \
     --n_ensembles $n_ensembles
