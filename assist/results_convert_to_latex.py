13936f0833ae546a6131a5844c38df407c24c4ec
Yinghao-Li <www6com2010@hotmail.com>
2023-08-21 13:56:34 -0400

    update latex generation

--- assist/results_convert_to_latex.py
+++ assist/results_convert_to_latex.py
@@ -19,22 +19,21 @@ from typing import Optional
 from muben.utils.macro import (
     CLASSIFICATION_METRICS,
     REGRESSION_METRICS,
+    MODEL_NAMES,
+    DATASET_NAMES,
+    FINGERPRINT_FEATURE_TYPES,
     dataset_mapping,
     metrics_mapping
 )
 
 
-MODEL_NAMES = [
-    "DNN-rdkit",
-    "ChemBERTa",
-    "GROVER",
-    "Uni-Mol"
-]
 model_mapping = {
     "DNN-rdkit": 'DNN',
     "ChemBERTa": "ChemBERTa",
     "GROVER": "GROVER",
-    "Uni-Mol": "Uni-Mol"
+    "Uni-Mol": "Uni-Mol",
+    "TorchMD-NET": "TorchMD-NET",
+    "GIN": "GIN"
 }
 CLASSIFICATION_UNCERTAINTY = [
     'none',
@@ -79,6 +78,55 @@ class Arguments:
     output_dir: Optional[str] = field(
         default="latex", metadata={"help": "Directory to save the latex tables."}
     )
+    dataset_names: Optional[str] = field(
+        default=None,
+        metadata={
+            "nargs": "*",
+            "help": "A list of dataset names."
+        }
+    )
+    model_names: Optional[str] = field(
+        default=None,
+        metadata={
+            "nargs": "*",
+            "help": "A list of model names."
+        }
+    )
+    feature_type: Optional[str] = field(
+        default="rdkit",
+        metadata={
+            "choices": FINGERPRINT_FEATURE_TYPES,
+            "help": "Feature type that the DNN model uses."
+        }
+    )
+    filename_prefix: Optional[str] = field(
+        default="tb3.result.dataset",
+        metadata={"help": "Prefix of the file names."}
+    )
+    label_prefix: Optional[str] = field(
+        default="apptb:result.dataset",
+        metadata={"help": "Prefix of the table labels."}
+    )
+
+    def __post_init__(self):
+        if self.model_names is None:
+            self.model_names = MODEL_NAMES
+        elif isinstance(self.model_names, str):
+            self.model_names: list[str] = [self.model_names]
+
+        for idx, model_name in enumerate(self.model_names):
+            if model_name == "DNN":
+                assert self.feature_type != 'none', ValueError("Invalid feature type for DNN!")
+                self.model_names[idx] = f"DNN-{self.feature_type}"
+
+        if self.dataset_names is None:
+            self.dataset_names: list[str] = DATASET_NAMES
+        elif self.dataset_names == "classification":
+            self.dataset_names: list[str] = CLASSIFICATION_DATASET
+        elif self.dataset_names == "regression":
+            self.dataset_names: list[str] = REGRESSION_DATASET
+        elif isinstance(self.dataset_names, str):
+            self.dataset_names: list[str] = [self.dataset_names]
 
 
 def main(args: Arguments):
@@ -87,15 +135,20 @@ def main(args: Arguments):
     # for classification datasets
     mapped_metrics = [metrics_mapping[m] for m in CLASSIFICATION_METRICS]
     for dataset in CLASSIFICATION_DATASET:
+        if dataset not in args.dataset_names:
+            continue
         dfs = list()
-        for model_name in MODEL_NAMES:
+        for model_name in args.model_names:
             result_file = op.join(score_path, f"{model_name}-{dataset}.csv")
             df = pd.read_csv(result_file, index_col='method')
             for metrics in CLASSIFICATION_METRICS:
-                df[metrics_mapping[metrics]] = [
-                    (f"{m:.4f}" if m == m else '-') + " " + (f"({s:.4f})" if s == s else '(-)')
-                    for m, s in zip(df[f'{metrics}-mean'], df[f'{metrics}-std'])
-                ]
+                try:
+                    df[metrics_mapping[metrics]] = [
+                        (f"{m:.4f}" if m == m else '-') + " $\\pm$ " + (f"{s:.4f}" if s == s else '-')
+                        for m, s in zip(df[f'{metrics}-mean'], df[f'{metrics}-std'])
+                    ]
+                except KeyError:
+                    df[metrics_mapping[metrics]] = ["- $\\pm$ -" for _ in df[f'roc-auc-mean']]
             df = df[mapped_metrics]
             df = df.reindex([f'{model_name}-{unc}' for unc in CLASSIFICATION_UNCERTAINTY])
             index_mapping = {f"{model_name}-{unc}": uncertainty_mapping[unc] for unc in CLASSIFICATION_UNCERTAINTY}
@@ -106,24 +159,29 @@ def main(args: Arguments):
 
         data_df = pd.concat(dfs)
         init_dir(args.output_dir, clear_original_content=False)
-        data_df.to_latex(op.join(args.output_dir, f"tb.result.dataset.{dataset}.tex"),
+        data_df.to_latex(op.join(args.output_dir, f"{args.filename_prefix}.{dataset}.tex"),
                          caption=f"Test results on {dataset_mapping[dataset]} "
-                                 f"in the format of ``metric mean (standard deviation)''.",
-                         label=f"apptb:result.dataset.{dataset}",
+                                 f"in the format of ``metric mean $\\pm$ standard deviation''.",
+                         label=f"{args.label_prefix}.{dataset}",
                          position='tbh')
 
     # for regression datasets
     mapped_metrics = [metrics_mapping[m] for m in REGRESSION_METRICS]
     for dataset in REGRESSION_DATASET:
+        if dataset not in args.dataset_names:
+            continue
         dfs = list()
-        for model_name in MODEL_NAMES:
+        for model_name in args.model_names:
             result_file = op.join(score_path, f"{model_name}-{dataset}.csv")
             df = pd.read_csv(result_file, index_col='method')
             for metrics in REGRESSION_METRICS:
-                df[metrics_mapping[metrics]] = [
-                    (f"{m:.4f}" if m == m else '-') + " " + (f"({s:.4f})" if s == s else '(-)')
-                    for m, s in zip(df[f'{metrics}-mean'], df[f'{metrics}-std'])
-                ]
+                try:
+                    df[metrics_mapping[metrics]] = [
+                        (f"{m:.4f}" if m == m else '-') + " $\\pm$ " + (f"{s:.4f}" if s == s else '-')
+                        for m, s in zip(df[f'{metrics}-mean'], df[f'{metrics}-std'])
+                    ]
+                except KeyError:
+                    df[metrics_mapping[metrics]] = ["- $\\pm$ -" for _ in df[f'rmse-mean']]
             df = df[mapped_metrics]
             df = df.reindex([f'{model_name}-{unc}' for unc in REGRESSION_UNCERTAINTY])
             index_mapping = {f"{model_name}-{unc}": uncertainty_mapping[unc] for unc in REGRESSION_UNCERTAINTY}
@@ -134,10 +192,10 @@ def main(args: Arguments):
 
         data_df = pd.concat(dfs)
         init_dir(args.output_dir, clear_original_content=False)
-        data_df.to_latex(op.join(args.output_dir, f"tb.result.dataset.{dataset}.tex"),
+        data_df.to_latex(op.join(args.output_dir, f"{args.filename_prefix}.{dataset}.tex"),
                          caption=f"Test results on {dataset_mapping[dataset]} "
-                                 f"in the format of ``metric mean (standard deviation)''.",
-                         label=f"apptb:result.dataset.{dataset}",
+                                 f"in the format of ``metric mean $\\pm$ standard deviation''.",
+                         label=f"{args.label_prefix}.{dataset}",
                          position='tbh')
     return None
 
