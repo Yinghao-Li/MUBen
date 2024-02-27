d4cc3265487a137eeca625a86bc0d9a77cca3f65
Yinghao-Li <www6com2010@hotmail.com>
2024-02-27 17:20:08 -0500

    remove redundant files

--- muben/train/__init__.py
+++ muben/train/__init__.py
@@ -1,7 +1,6 @@
-from .trainer_3d import Trainer as Trainer3D
 from .trainer_grover import Trainer as TrainerGrover
 from .trainer_unimol import Trainer as TrainerUnimol
 from .trainer import Trainer
 
 
-__all__ = ["Trainer3D", "TrainerGrover", "TrainerUnimol", "Trainer"]
+__all__ = ["TrainerGrover", "TrainerUnimol", "Trainer"]
